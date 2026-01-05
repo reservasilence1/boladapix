c20cd609296d95fc4e4b802375bd8d6686380bbc
reservasilence1 <reservasilence1@gmail.com>
2026-01-05 04:33:34 -0300

    Update pix.js

--- api/pix.js
+++ api/pix.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
     // SUA URL de criação (POST)
     const DUTTYFY_CREATE_URL =
-      "https://app.duttyfy.com.br/api-pix/vkf5TMM12Qn32cTNDGV-yhIaKC6tEYDIPu2SFKHNKNYgl_s7JZg4sArNGdWpnF4PtBeiz5FV2-kWigfJsU3vNA";
+      "https://app.duttyfy.com.br/api-pix/FuDhN9MbjCXxoDWW9QWFtRdxl_UYkxJ1GQ1N19nwIJFQZbJNlj5Gkx0955uo81bDTRqteD9Y025dDSa92tC86A";
 
     const r = await fetch(DUTTYFY_CREATE_URL, {
       method: "POST",
